d402a4cd11aebfc48cd6270f34e0bdcafc85f427
zada-fi <128002274+zada-fi@users.noreply.github.com>
2023-03-30 23:22:41 +0800

    Add files via upload
    init code

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# zada-interface-sdk
+# Zada-Interface SDK
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+
+## Running tests
+
+To run the tests, follow these steps. You must have at least node v10 and [yarn](https://yarnpkg.com/) installed.
+
+First clone the repository:
+
+```sh
+git clone https://github.com/zada-fi/zada-interface-sdk
+```
+
+Move into the zada-interface-sdk working directory
+
+```sh
+cd zada-interface-sdk/
+```
+
+Install dependencies
+
+```sh
+yarn install
+```
+
+Run tests
+
+```sh
+yarn test
+```
+
+You should see output like the following:
+
+```sh
+yarn run v1.22.4
+$ tsdx test
+ PASS  test/constants.test.ts
+ PASS  test/pair.test.ts
+ PASS  test/fraction.test.ts
+ PASS  test/miscellaneous.test.ts
+ PASS  test/entities.test.ts
+ PASS  test/trade.test.ts
+
+Test Suites: 1 skipped, 6 passed, 6 of 7 total
+Tests:       3 skipped, 82 passed, 85 total
+Snapshots:   0 total
+Time:        5.091s
+Ran all test suites.
+✨  Done in 6.61s.
+```
---
+++ src/constants.ts
@@ -0,0 +1,51 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  SCROLL_ALPHA = 534353
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0x072d29B5ea6d681BAD7E3D7B8879b27C65EAE08c'
+
+export const INIT_CODE_HASH = '0x4ea6e061e54920cb0b90081e1c557194b96045d688c9f4cf27ec5f48c42efeea'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
